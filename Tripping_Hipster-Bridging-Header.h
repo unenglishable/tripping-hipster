8131529863cbfab92ac4d89567da60e14a71bbc0
unenglishable <unenglishable@gmail.com>
2015-01-14 11:38:52 -1000

    add bridging header

---
+++ Tripping_Hipster-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Tripping_Hipster-Bridging-Header.h
+//  tripping-hipster
+//
+//  Created by Bronson Oka on 1/13/15.
+//  Copyright (c) 2015 AM. All rights reserved.
+//
+
+#ifndef tripping_hipster_Tripping_Hipster_Bridging_Header_h
+#define tripping_hipster_Tripping_Hipster_Bridging_Header_h
+
+#import "Pods/SBJson/src/main/objc/SBJson4.h"
+
+#endif
